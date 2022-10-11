b552d6df0d4dc755289d14a9e9581bf575e13e93
Ilias Bounsir <74919012+IBOCATA@users.noreply.github.com>
2022-10-11 13:13:42 +0100

    Create manage.py

---
+++ manage.py
@@ -0,0 +1,4 @@
+django-admin check auth admin myapp
+django-admin check --tag models --tag compatibility
+django-admin check --database default --database other
+django-admin check --deploy --settings=production_settings
