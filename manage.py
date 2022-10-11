ca7ebf3ebd24f112e9538804e9bae5a2067e1781
Ilias Bounsir <74919012+IBOCATA@users.noreply.github.com>
2022-10-11 14:38:26 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -22,3 +22,30 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(base_query.format(table=existing_table)) for existing_table in existing_tables
     ]
+$ python3 --version
+Python 3.9
+$ sudo aptitude install gdal-bin libgdal-dev
+$ sudo aptitude install python3-gdal
+$ sudo aptitude install binutils libproj-dev
+aptitude install binutils libproj-dev
+$ docker run --name=postgis -d -e POSTGRES_USER=user001 -e POSTGRES_PASS=123456789 -e POSTGRES_DBNAME=gis -p 5432:5432 kartoza/postgis:9.6-2.4
+$ python3 -m venv env
+(env) $
+$ source env/bin/activate
+$ pip install django
+$ django-admin.py startproject seismic 3d
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gis',
+        'USER': 'user001',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+$ pip install psycopg2-binary
+INSTALLED_APPS = [
+    # [...]
+    'django.contrib.gis'
+]
