c483ef5f39acea8b3c73dfce0e981d74fd885467
Ilias Bounsir <74919012+IBOCATA@users.noreply.github.com>
2022-10-11 13:25:45 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,3 +2,23 @@ django-admin check auth admin myapp
 django-admin check --tag models --tag compatibility
 django-admin check --database default --database other
 django-admin check --deploy --settings=production_settings
+from django.db import migrations
+from django.db import connection
+
+
+class Migration(migrations.Migration):
+    db_cursor = connection.cursor()
+    check_exists_query = "SELECT relname FROM pg_class WHERE relname=%s;"
+    base_query = "DELETE FROM {table} WHERE condition;"
+    tables = [tables]
+    existing_tables = []
+
+    for table in tables:
+        db_cursor.execute(check_exists_query, [table])
+        result = db_cursor.fetchone()
+        if result:
+            existing_tables.append(table)
+
+    operations = [
+        migrations.RunSQL(base_query.format(table=existing_table)) for existing_table in existing_tables
+    ]
