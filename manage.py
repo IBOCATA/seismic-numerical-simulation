395382f040550a23d8ece7f98ef134eb476993fb
Ilias Bounsir <74919012+IBOCATA@users.noreply.github.com>
2022-10-16 15:12:36 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,27 +1,2 @@
-dtables = [tables]
-    existing_tables = []
-
-    for table in tables:
-        db_cursor.execute(check_exists_query, [table])
-        result = db_cursor.fetchone()
-        if result:
-           existing_tables.append(table)
-
-    operations = [
-        migrations.RunSQL(base_query.format(table=existing_table)) for existing_table in existing_tables
-(env) $
-$ source env/bin/activate
-$ pip install django
-$ django-admin.py startproject seismic 3d
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'gis',
-        'USER': 'user001',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '8000'
-    }
-}
 
  
