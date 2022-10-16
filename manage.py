f611481668389b1d2065579b18686394a93279c5
Ilias Bounsir <74919012+IBOCATA@users.noreply.github.com>
2022-10-16 17:27:41 +0200

    Create django.yml (#1)
    * Create django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Create Requirement.txt
    
    * Create manage.py
    
    * Update manage.py
    
    * Update manage.py
    
    * Update manage.py (#6)
    
    * Update manage.py
    
    * Update manage.py
    
    * Update manage.py
    
    * Update manage.py
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Update manage.py
    
    * Update manage.py
    
    * Update manage.py
    
    * Update django.yml
    
    * Update manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,12 @@
-django-admin check --deploy --settings=production_settings
+pipenv install mysql-connector-python
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, Country VARCHAR(255), City VARCHAR(255), PGA FLOAT, PGV FLOAT)")
